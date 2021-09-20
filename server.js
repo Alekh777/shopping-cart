f29609399db217809e5b1b010cbe875236bbd75e
Alekh Kumar <alekhkumar777@gmail.com>
2021-09-21 00:58:14 +0530

    Create routing with express

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
+const index = require('./routes/api/index'); // or ('./routes/api')
 
 app.use('/', express.static(__dirname + '/public'))
 
+app.use('/api', index.route); // Since index is an object which contains route, ( module.exports = {route}; ). If it was
+                              // module.exports = route, then we should have written -> app.use('/api', index), since it is a value not object.
 app.listen(4444, ()=>{
     console.log("Server started at http://localhost:4444/");
 })
