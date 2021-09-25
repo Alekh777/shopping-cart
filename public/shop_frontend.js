0447e6e5611120bb4ae06ef4acd4e24d267cf57f
Alekh Kumar <alekhkumar777@gmail.com>
2021-09-26 01:20:58 +0530

    Fetch data from database then display it on frontend

---
+++ public/shop_frontend.js
@@ -0,0 +1,32 @@
+function fetchProducts(done){
+    $.get('/api/products', function (data){
+        done(data)
+    })
+}
+
+function createProductCard(product){
+    return $(`
+    <div class="col-4 card mx-2 p-3">
+    <h4 class="product-name">${product.name}</h4>
+    <div class="product-manufacturer">${product.manufacturer}</div>
+    <div class="row">
+        <div class="col mx-3 px-4">
+            <b>Rs. ${product.price}</b>
+        </div>
+        <button class="btn btn-outline-dark mx-3 px-5">Buy</button>
+    </div>
+    <br>
+</div>
+    `)
+}
+
+$(function(){
+    let productlist = $('#product-list')
+
+    fetchProducts(function(products){
+        productlist.empty();
+        for(product of products){
+            productlist.append(createProductCard(product))
+        }
+    })
+})
